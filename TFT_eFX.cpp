2a66dca6efeaff3c094287f25f9d49ee301b405f
Bodmer <bodmer@anola.net>
2018-11-16 00:34:56 +0000

    Correct function call name

--- TFT_eFX.cpp
+++ TFT_eFX.cpp
@@ -44,7 +44,7 @@ void TFT_eFX::drawBezier(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t
     t = (x0 * x2 - x1 * x1) * t / (x0 - x1);
     x = floor(t + 0.5); y = floor(r + 0.5);
     r = (y1 - y0) * (t - x0) / (x1 - x0) + y0;
-    drawBezier(x0, y0, x, floor(r + 0.5), x, y, color);
+    drawBezierSegment(x0, y0, x, floor(r + 0.5), x, y, color);
     r = (y1 - y2) * (t - x2) / (x1 - x2) + y2;
     x0 = x1 = x; y0 = y; y1 = floor(r + 0.5);
   }
@@ -54,7 +54,7 @@ void TFT_eFX::drawBezier(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t
     t = (y0 * y2 - y1 * y1) * t / (y0 - y1);
     x = floor(r + 0.5); y = floor(t + 0.5);
     r = (x1 - x0) * (t - y0) / (y1 - y0) + x0;
-    drawBezier(x0, y0, floor(r + 0.5), y, x, y, color);
+    drawBezierSegment(x0, y0, floor(r + 0.5), y, x, y, color);
     r = (x1 - x2) * (t - y2) / (y1 - y2) + x2;
     x0 = x; x1 = floor(r + 0.5); y0 = y1 = y;
   }
